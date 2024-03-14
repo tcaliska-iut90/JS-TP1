50e0a8fda0ba8dfe41c6738535618446f21a4cba
tcaliska-iut90 <turker.caliskan@edu.univ-fcomte.fr>
2024-03-14 13:39:17 +0100

    Test

--- hello.js
+++ hello.js
@@ -1,4 +1,9 @@
-function calculer(masse, taille){
-    var imc = masse / (taille * taille);
-    return imc;
+function calculer(){
+    var masse;
+    var taille;
+    document.getElementById("masse").src = masse;
+    document.getElementById("masse").src = taille;
+    var imc;
+    imc = masse / (taille * taille);
+    alert(imc);
 }
